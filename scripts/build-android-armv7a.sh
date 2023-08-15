a5260d780b8de165b33631f87a5006eb75919e0e
SLam4er <86654605+SLam4er@users.noreply.github.com>
2023-08-15 17:43:27 +0300

    .

--- scripts/build-android-armv7a.sh
+++ scripts/build-android-armv7a.sh
@@ -6,6 +6,6 @@ export ANDROID_NDK_HOME=$PWD/android-ndk-r10e/
 wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.1.0/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz -o /dev/null
 tar -xf clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
 export PATH="$PWD/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04/bin:$PATH"
-./waf configure -T release --build-game=cstrike --prefix=Srceng-mod-launcher/android --android=armeabi-v7a-hard,host,21 --target=../armeabi-v7a --enable-opus --togles --disable-warns &&
-./waf install --target=client,server,engine --strip
+./waf configure -T release --build-game=hl2 --prefix=Srceng-mod-launcher/android --android=armeabi-v7a-hard,host,21 --target=../armeabi-v7a --enable-opus --togles --disable-warns &&
+./waf install  --strip
 rm Srceng-mod-launcher/android/lib/armeabi-v7a/README.md
