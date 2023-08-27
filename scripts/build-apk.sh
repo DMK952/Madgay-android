20efeaf95b10416e1a608e75c974b79b4028cd35
SLam4er <86654605+SLam4er@users.noreply.github.com>
2023-08-27 20:52:38 +0300

    /

--- scripts/build-apk.sh
+++ scripts/build-apk.sh
@@ -2,6 +2,5 @@ cd Srceng-mod-launcher
 git clone https://gitlab.com/LostGamer/android-sdk
 export ANDROID_SDK_HOME=$PWD/android-sdk
 git pull
-./build-vpk.sh
 ./waf configure -T release &&
 ./waf build
